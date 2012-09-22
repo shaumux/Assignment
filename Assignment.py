e9c335742d6a9b3f5bfc041415505772c84b3ccf
Shaumyadeep Chaudhuri <shaumux@ubuntu.(none)>
2012-09-21 17:01:42 -0700

    Tested on Ubuntu 12.04

--- Assignment.py
+++ Assignment.py
@@ -21,11 +21,11 @@ def chkPackageInstStatus(cache,packageName):
 
 def installPackage(cache,packageName):
     pkg = cache[packageName]
-    print packageName,' is trusted:', pkg.candidate.origins[0].trusted
+    print '%s is trusted:%s'%(packageName,pkg.candidate.origins[0].trusted)
     pkg.mark_install()
-    print packageName,' is marked for install:', pkg.marked_install
-    print packageName,' is (summary):', pkg.candidate.summary
-    cache.commit()
+    print '%s is marked for install:%s'%(packageName,pkg.marked_install)
+    print '%s is %s:'%(packageName, pkg.candidate.summary)
+
     
 def updateSourcesList():
     with open("/etc/apt/sources.list.d/ondrej-php5-precise.list", "w") as php5Source:
@@ -51,21 +51,21 @@ def domainConf(dmn):
             configured=False
     
     if not configured:
-        with open("/etc/hosts", "w") as hosts:
+        with open("/etc/hosts", "a") as hosts:
             hosts.write("%s %s"%(localip,dmn))
-    os.symlink("/etc/nginx/sites-available/"+dmn, "/etc/nginx/sites-enabled/")
+    
     
     
 def nginxConf(dmn):
     if not os.path.exists("/etc/nginx/sites-available/%s"%dmn):
-        with open("/etc/nginx/sites-available/%s"%dmn, "w") as conf:
-            conf.write("""server {
-    server_name %s *.%s;
-    access_log   /var/log/nginx/$dnm.access.log;
-    error_log    /var/log/nginx/$dnm.error.log;
+        with open("/etc/nginx/sites-available/%s"%(dmn), "w") as conf:
+            conf.write("""server{
+	server_name %s *.%s;
+	access_log   /var/log/nginx/%s.access.log;
+	error_log    /var/log/nginx/%s.error.log;
 
-    index index.php;
-        root /var/www/$dnm/htdocs;
+	index index.php;
+        root /var/www/%s/htdocs;
         
 
         location / {
@@ -74,51 +74,54 @@ def nginxConf(dmn):
 
         location ~ \.php$ {
                 include fastcgi_params;
-        fastcgi_pass 127.0.0.1:9000;
+		fastcgi_pass unix:/var/run/php5-fpm.sock;
         }
-}"""%(dmn,dmn))
+}"""%(dmn,dmn,dmn,dmn,dmn))
+	os.symlink("/etc/nginx/sites-available/%s"%dmn, "/etc/nginx/sites-enabled/%s"%dmn)
 
 def wpconf(dmn):
-    if not os.path.exists("/var/www/"+dmn+"/htdocs/"):
-        os.makedirs("/var/www/"+dmn+"/htdocs/",0755)
     print "Fetching latest version of Wordprerss"
     f = urllib2.urlopen("http://wordpress.org/latest.zip")
     with open("latest.zip", "wb") as code:
         code.write(f.read())
-    zipfile.ZipFile.extractall("/var/www/"+dmn+"/htdocs/")
-    shutil.move("/var/www/"+dmn+"/htdocs/wordpress", "/var/www/"+dmn+"/htdocs/")
-    uname=raw_input("Please Enter the Username of an existing user for database login")
-    password=getpass.getpass(prompt="Please Enter the password of an existing user for database login")    
-    p = subprocess.Popen(['mysql', '-u', uname, '-p'+password, '-e \'create database examplecom_db\''], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    out, err =p.communicate
-    print out
-    print err
-    #shutil.copy("/var/www/"+dmn+"/htdocs/wp-config-sample.php", "/var/www/"+dmn+"/htdocs/wp-config.php")
+    zf=zipfile.ZipFile("latest.zip")
+    zf.extractall("/var/www/%s/"%dmn)
+    shutil.copytree("/var/www/%s/wordpress"%dmn, "/var/www/%s/htdocs/"%dmn)
+    shutil.rmtree("/var/www/%s/wordpress"%dmn)
+    uname=raw_input("Please Enter the Username of an existing user for database login:")
+    password=getpass.getpass(prompt="Please Enter the password of an existing user for database login:")
+    os.system("mysql -u %s -p%s -e 'create database examplecom_db'"%(uname,password))
     salt = urllib2.urlopen("https://api.wordpress.org/secret-key/1.1/salt/")
     with open("/var/www/"+dmn+"/htdocs/wp-config-sample.php", "r") as sample:
         lines = sample.readlines()
         with open("/var/www/"+dmn+"/htdocs/wp-config.php", "w") as config:
             for line in lines:
                 config.write(re.sub(r'database_name_here', 'examplecom_db', line))
-                config.write(re.sub(r'username_here', uname, line))
+    with open("/var/www/%s/htdocs/wp-config.php"%dmn, "r") as config:
+	lines = config.readlines()
+    	with open("/var/www/%s/htdocs/wp-config.php"%dmn, "w") as config:
+            for line in lines:
+	        config.write(re.sub(r'username_here', uname, line))
+    with open("/var/www/%s/htdocs/wp-config.php"%dmn, "r") as config:
+	lines = config.readlines()
+    	with open("/var/www/%s/htdocs/wp-config.php"%dmn, "w") as config:
+	    for line in lines:
                 config.write(re.sub(r'password_here', password, line))
-                config.write(re.sub(r'#@+.*?#@-', salt.read(), line,flags=re.DOTALL))
-    for root, dirs, files in os.walk("/var/www/"+dmn):
-        for momo in dirs:
-            os.chown(os.path.join(root, momo), pwd.getpwnam("www-data")[3], grp.getgrnam("www-data"))
-        for momo in files:
-            os.chown(os.path.join(root, momo), pwd.getpwnam("www-data")[3], grp.getgrnam("www-data"))
+    with open("/var/www/%s/htdocs/wp-config.php"%dmn, "r") as config:
+	block = config.read()
+        with open("/var/www/%s/htdocs/wp-config.php"%dmn, "w") as config:
+                config.write(re.sub(r'/\*\*\#@\+.*?\#@-\*/', salt.read(), block,flags=re.DOTALL))
+    os.system("chown -R www-data:www-data /var/www/%s"%dmn)
             
 def restart(service):
-    p = subprocess.Popen(['service', 'restart', service], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    out, err =p.communicate
+    p = subprocess.Popen(['service', service, 'restart'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    out, err =p.communicate()
     print out
     print err
 
 
 def main():
     sanitycheck()
-    print
     cache=apt.Cache()
     dmn=raw_input("Please Enter Domain Name:")
     reqPackages=("php5-cgi","php5-mysql","php5-fpm","nginx","mysql-server")
@@ -132,11 +135,12 @@ def main():
         cache=updateCache(cache)
         
     for package in reqPackages:
-        print "Checking Package Install Status:"
-        if chkPackageInstStatus(cache,package):
+        print "Checking %s Install Status:"%package
+        if not chkPackageInstStatus(cache,package):
             installPackage(cache,package)
+    cache.commit()
     domainConf(dmn)
-    nginxConf()
+    nginxConf(dmn)
     wpconf(dmn)
     restart("php5-fpm")
     restart("mysql")
